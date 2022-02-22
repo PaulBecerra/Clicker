77a644e92c6acdb40921a0c18f93ff7ac8cda67f
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-02-22 00:19:03 -0700

    working

--- app/src/main/java/becerra/paul/clicker/MainActivity.kt
+++ app/src/main/java/becerra/paul/clicker/MainActivity.kt
@@ -6,12 +6,14 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     var count: Int = 0
+    lateinit var countName: String
     lateinit var btnClicker: Button
     lateinit var textViewCuenta: TextView
 
@@ -24,20 +26,52 @@ class MainActivity : AppCompatActivity() {
 
         val btnDecrease: Button = findViewById(R.id.btnRestar)
         val btnClear: Button = findViewById(R.id.btnClear)
+        val btnSave: Button = findViewById(R.id.btn_save)
+
+        val editText_CountName: EditText = findViewById(R.id.EditText_CountName)
+        btnSave.setOnClickListener{
+            val input = editText_CountName.text.toString()
+
+            if (input.isNullOrEmpty()) {
+                warning()
+                return@setOnClickListener
+            }
+
+            countName = input
+            textViewCuenta.setText("$count $countName")
+        }
 
         btnClicker.setOnClickListener{
             count++
-            textViewCuenta.setText("$count")
+            textViewCuenta.setText("$count $countName")
         }
 
         btnDecrease.setOnClickListener{
             count--
-            textViewCuenta.setText("$count")
+            textViewCuenta.setText("$count $countName")
         }
 
         btnClear.setOnClickListener{
             clear()
         }
+    }
+
+    fun warning(){
+        val alertDialog: AlertDialog? = this?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User clicked OK button
+                    })
+            }
+            builder?.setMessage(R.string.dialog_inputEmpty)
+                .setTitle(R.string.dialog_title)
+
+            // Create the AlertDialog
+            builder.create()
+        }
+        alertDialog?.show()
     }
 
     fun clear(){
@@ -48,7 +82,7 @@ class MainActivity : AppCompatActivity() {
                     DialogInterface.OnClickListener { dialog, id ->
                         // User clicked OK button
                         count = 0
-                        textViewCuenta.setText("$count")
+                        textViewCuenta.setText("$count $countName")
                     })
                 setNegativeButton(R.string.cancel,
                     DialogInterface.OnClickListener { dialog, id ->
@@ -69,6 +103,7 @@ class MainActivity : AppCompatActivity() {
         val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
         val edit = sharedPref.edit()
         edit.putInt("key_count",count)
+        edit.putString("key_countName",countName)
         edit.apply()
     }
 
@@ -76,6 +111,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
         count = sharedPref.getInt("key_count", 0)
-        textViewCuenta.setText("$count")
+        countName = sharedPref.getString("key_countName","clicks").toString()
+        textViewCuenta.setText("$count $countName")
     }
 }
