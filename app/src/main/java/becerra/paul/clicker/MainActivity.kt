af7bc57bedf4d0ea9bd48e66ca8b3251a7aff06d
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-02-21 22:06:56 -0700

    alert dialog

--- app/src/main/java/becerra/paul/clicker/MainActivity.kt
+++ app/src/main/java/becerra/paul/clicker/MainActivity.kt
@@ -1,5 +1,7 @@
 package becerra.paul.clicker
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,34 +11,58 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     var count: Int = 0
-    lateinit var btnClick: Button
+    lateinit var btnClicker: Button
     lateinit var textViewCuenta: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnClick = findViewById(R.id.btn_clicker)
+        btnClicker = findViewById(R.id.btn_clicker)
         textViewCuenta = findViewById(R.id.textViewCount)
 
-        val btnRestar: Button = findViewById(R.id.btnRestar)
+        val btnDecrease: Button = findViewById(R.id.btnRestar)
         val btnClear: Button = findViewById(R.id.btnClear)
 
-        btnClick.setOnClickListener{
+        btnClicker.setOnClickListener{
             count++
             textViewCuenta.setText("$count")
         }
 
-        btnRestar.setOnClickListener{
+        btnDecrease.setOnClickListener{
             count--
             textViewCuenta.setText("$count")
         }
 
         btnClear.setOnClickListener{
-            count = 0
-            textViewCuenta.setText("$count")
+            clear()
         }
     }
+
+    fun clear(){
+        val alertDialog: AlertDialog? = this?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User clicked OK button
+                        count = 0
+                        textViewCuenta.setText("$count")
+                    })
+                setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            }
+            builder?.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title)
+
+            // Create the AlertDialog
+            builder.create()
+        }
+        alertDialog?.show()
+    }
+
 
     override fun onPause() {
         super.onPause()
